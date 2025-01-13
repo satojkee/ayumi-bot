3f218179fa6aaff26734c006505f705ef0089d6e
satojkee <satojkee@gmail.com>
2025-01-13 12:36:27 +0200

    fix: messages.py removed

--- ayumi/messages.py
+++
@@ -1,6 +0,0 @@
-# For logging
-INFO_RESPONSE_SENT: str = 'Successfully responded. Chat: {}; User: {}; Query: "{}"; Response: "{}"'
-ERROR_REQUEST_FAILED: str = 'Request failed. Chat: {}; User: {}; Query: "{}"; Reason: "{}"'
-
-# For chat
-CHAT_RESPONSE_FAILED: str = 'Sorry, something went wrong. Please try again.'
