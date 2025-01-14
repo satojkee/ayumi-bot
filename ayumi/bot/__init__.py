1d5ba57439adc2309c452a7f74a72b13f5387c47
satojkee <satojkee@gmail.com>
2025-01-14 11:14:58 +0200

    fix: running-config displays on start_ayumi call, not package init

--- ayumi/bot/__init__.py
+++ ayumi/bot/__init__.py
@@ -1,6 +1,9 @@
+import json
+
 from telebot.async_telebot import AsyncTeleBot
 
-from ayumi.config import TELEGRAM_TOKEN
+from ayumi import logger
+from ayumi.config import TELEGRAM_TOKEN, app_config
 
 
 __all__ = (
@@ -15,6 +18,9 @@ session = AsyncTeleBot(TELEGRAM_TOKEN)
 def start_ayumi() -> None:
     """Use it to start telegram polling."""
     import asyncio
+
+    logger.debug(f'starting {__name__}')
+    logger.debug(f'running configuration: {json.dumps(app_config, indent=2)}')
 
     asyncio.run(session.infinity_polling())
 
